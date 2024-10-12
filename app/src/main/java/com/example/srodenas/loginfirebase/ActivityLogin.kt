2c6f5a3af6339baedc69f87fe2664e9565fda7e2
srodenas <srodher115@g.educaand.es>
2024-10-12 22:18:02 +0200

    Creación, logueo, recuperacion contraseña y verificacion de creacion de cuenta

--- app/src/main/java/com/example/srodenas/loginfirebase/ActivityLogin.kt
+++ app/src/main/java/com/example/srodenas/loginfirebase/ActivityLogin.kt
@@ -1,18 +1,30 @@
 package com.example.srodenas.loginfirebase
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthEmailException
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.FirebaseAuthInvalidUserException
+import com.google.firebase.auth.FirebaseAuthUserCollisionException
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException
+import com.google.firebase.auth.auth
 
 class ActivityLogin : AppCompatActivity() {
     private lateinit var btnLogin : Button
     private lateinit var btnRegister : Button
+    private lateinit var btnRecoverPass : Button
     private lateinit var editUser : EditText
     private lateinit var editPassword : EditText
+    private lateinit var auth : FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,18 +44,113 @@ class ActivityLogin : AppCompatActivity() {
     private fun init(){
         btnLogin = findViewById(R.id.btn_login_in_login)
         btnRegister = findViewById(R.id.btn_register_in_login)
+        btnRecoverPass = findViewById(R.id.btn_recover_pass)
         editUser = findViewById(R.id.edit_user_login)
         editPassword = findViewById(R.id.edit_pass_login)
+        auth = Firebase.auth  //Creamos el objeto de autentication.
     }
 
 
     private fun start() {
         btnLogin.setOnClickListener{
+            val user = editUser.text.toString()
+            val pass = editPassword.text.toString()
 
+            if (user.isNotEmpty() && pass.isNotEmpty())
+                startLogin(user, pass){
+                    result, msg ->
+                        Toast.makeText(this@ActivityLogin, msg, Toast.LENGTH_LONG).show()
+                        if (result){
+                            intent = Intent(this@ActivityLogin, MainActivity::class.java)
+                            startActivity(intent)
+                            finish()
+                        }
+                }
+            else
+                Toast.makeText(this, "Tienes algún campo vacío", Toast.LENGTH_LONG).show()
         }
 
         btnRegister.setOnClickListener{
-
+            intent = Intent (this, ActivityRegister::class.java)
+            startActivity(intent)
         }
+
+        btnRecoverPass.setOnClickListener{
+            val user = editUser.text.toString()
+            if (user.isNotEmpty())
+                recoverPassword(user){
+                    result, msg ->
+                        Toast.makeText(this@ActivityLogin, msg, Toast.LENGTH_LONG).show()
+                         if (!result)
+                             editUser.setText("")
+                }
+            else
+                Toast.makeText(this, "Debes rellenar el campo email", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    /*
+    Cuando queramos la recuperación de password, al mandar un email
+    válido, firebase lo saca como satisfactorio exista o no el usuario
+    en firebase. Eso lo hace, porque no quiere que compruebe, si un usuario
+    existe en su bbdd. Es lógico.
+     */
+    private fun recoverPassword(email : String, onResult: (Boolean, String)->Unit) {
+        auth.sendPasswordResetEmail(email)
+            .addOnCompleteListener{
+                taskResetEmail ->
+                    if (taskResetEmail.isSuccessful){
+                        onResult (true, "Acabamos de enviarte un email con la nueva password")
+                    }else{
+                        var msg = ""
+                        try{
+                            throw taskResetEmail.exception?:Exception("Error de reseteo inesperado")
+                        }catch (e : FirebaseAuthInvalidCredentialsException){
+                            msg = "El formato del email es incorrecto"
+                        }catch (e: Exception){
+                            msg = e.message.toString()
+                        }
+                        onResult(false, msg)
+
+
+                    }
+            }
+
+
+    }
+
+    private fun startLogin(user: String, pass: String, onResult: (Boolean, String) -> Unit) {
+        auth.signInWithEmailAndPassword(user, pass)
+            .addOnCompleteListener {
+                taskAssin ->
+                    var msg = ""
+                    if (taskAssin.isSuccessful){
+                        //debemos comprobar si el usuario ha verificado el email
+                        val posibleUser = auth.currentUser
+                        if (posibleUser?.isEmailVerified == true){
+                            onResult ( true, "Usuario Logueado satisfactoriamente")
+                        }else{
+                            auth.signOut() //hay que desloguearse, porque no ha verificado.
+                            onResult (false, "Debes verificar tu correo antes de loguearte")
+                        }
+                    }else{
+                        try {
+                            throw taskAssin.exception?: Exception("Error desconocido")
+                        }catch (e: FirebaseAuthInvalidUserException){
+                            msg = "El usuario tiene problemas por haberse borrado o desabilitado"
+                        }catch (e: FirebaseAuthInvalidCredentialsException){
+                            msg = if (e.message?.contains("There is no user record corresponding to this identifier") == true){
+                                "El usuario no existe"
+                            }else "contraseña incorrecta"
+
+                        }catch (e: Exception){
+                            msg = e.message.toString()
+                        }
+
+                        onResult (false, msg)  //genérico.
+                    }
+
+            }
+
     }
 }
--- app/src/main/java/com/example/srodenas/loginfirebase/ActivityRegister.kt
+++ app/src/main/java/com/example/srodenas/loginfirebase/ActivityRegister.kt
@@ -1,14 +1,21 @@
 package com.example.srodenas.loginfirebase
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthException
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.FirebaseAuthUserCollisionException
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException
 import com.google.firebase.auth.auth
 
 
@@ -21,6 +28,7 @@ class ActivityRegister : AppCompatActivity() {
     private lateinit var editRepeatPassword : EditText
     private lateinit var auth : FirebaseAuth  //para autenticarme en firebase
 
+    //pruebasdam24.a@gmail.com
 
 
 
@@ -45,17 +53,99 @@ class ActivityRegister : AppCompatActivity() {
         editPassword = findViewById(R.id.edit_pass_register)
         editRepeatPassword = findViewById(R.id.pass_register_repeat_in_register)
 
-        auth = Firebase.auth
+        auth = Firebase.auth  //Creamos nuestro objeto de autenticación
+
     }
 
 
     private fun start() {
         btnRegister.setOnClickListener{
-
+            val email = editUser.text.toString()
+            val pass = editPassword.text.toString()
+            val repeatPass = editRepeatPassword.text.toString()
+            if (pass != repeatPass
+                || email.isEmpty()
+                || pass.isEmpty()
+                || repeatPass.isEmpty())
+                    Toast.makeText(this, "Campos vacíos y/o password diferentes", Toast.LENGTH_LONG).show()
+            else{
+                //todo correcto, vamos a verificar el registro.
+                /*
+                    Supongo que this, hará referencia al objeto Button, no al Activity.
+                     Cuando creamos una lambda para una llamada de órden superior, el contexto
+                     no tiene porqué ser el Activity. Hay que curarse en salud. registerUser, es un
+                     método mío, no predefinido como el caso del .setOnClickListener de un Buttom, que
+                     en este caso, el this hace referencia al activity, no al buttom.
+                     Cuando creemos una lambda sin parámetros, cuidado con el this porque no es el Activity.
+                */
+                registerUser (email, pass){
+                    result, msg ->
+                        Toast.makeText(this@ActivityRegister, msg, Toast.LENGTH_LONG).show()
+                        if (result)
+                            startActivityLogin()
+                }
+            }
         }
 
         btnLastRegister.setOnClickListener{
-
+            // Como la lambda tiene un parámetro view, el this es el Activity
+            view->
+                val intent = Intent (this, ActivityLogin::class.java)
+                startActivity(intent)
+                finish()
         }
+    }
+
+    private fun startActivityLogin() {
+        //Tengo que lanzar un intent con el Activity a loguear.
+        val intent = Intent(this, ActivityLogin::class.java)
+        startActivity(intent)
+        finish() //No quiero que sigua el Activity del registro.
+
+    }
+
+    private fun registerUser(email: String, pass: String, onResult: (Boolean, String) -> Unit) {
+        auth.createUserWithEmailAndPassword(email, pass)
+            .addOnCompleteListener(this){
+                taskAssin->
+                    if (taskAssin.isSuccessful){
+                        //enviaremos un email de confirmación
+                        val user = auth.currentUser
+                        user?.sendEmailVerification()
+                            ?.addOnCompleteListener{
+                                taskVerification ->
+                                    var msg = ""
+                                    if (taskVerification.isSuccessful)
+                                        msg = "Usuario Registrado Ok. Verifique su correo"
+                                    else
+                                        msg = "Usuario Registrado Ok. ${taskVerification.exception?.message}"
+                                auth.signOut() //tiene que verificar antes el email
+                                onResult(true, msg)
+                            }
+                            ?.addOnFailureListener{
+                                exception->
+                                    Log.e("ActivityRegister", "Fallo al enviar correo de verificación: ${exception.message}")
+                                    onResult(false, "No se pudo enviar el correo de verificación: ${exception.message}")
+                            }
+
+                    }else{
+                        try{
+                            throw taskAssin.exception ?:Exception ("Error desconocido")
+                        } catch (e: FirebaseAuthUserCollisionException){
+                            onResult (false, "Ese usuario ya existe")
+                        }catch (e: FirebaseAuthWeakPasswordException){
+                            onResult (false, "La contraseña es débil: ${e.reason}")
+                        }
+                        catch (e: FirebaseAuthInvalidCredentialsException){
+                            onResult (false, "El email proporcionado, no es válido")
+                        }
+                        catch (e: Exception){
+                            onResult (false, e.message.toString())
+                        }
+
+                    }
+            }
+
+
     }
 }
